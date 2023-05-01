1842797108391a6bc5644c0d9717f40cd8f5012a
Hanapibagas <hanafibagas4@gmail.com>
2023-05-01 20:55:49 +0800

    .

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"apistartup/user"
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/db_backend_golang?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -17,16 +24,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("selamat connection ke database telah berhasil")
-
 	var users []user.User
-
 	db.Find(&users)
 
-	for _, user := range users {
-		fmt.Println("============")
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("============")
-	}
+	c.JSON(http.StatusOK, users)
 }
